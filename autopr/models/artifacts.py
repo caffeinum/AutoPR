072b68ec23ebfce2284193a5f4c95e48322b12c3
Rafael Irgolic <hello@irgolic.com>
2023-03-30 23:09:17 +0200

    events: Introduce PR comment and code review

--- autopr/models/artifacts.py
+++ autopr/models/artifacts.py
@@ -30,4 +30,31 @@ class Issue(Thread):
         )
 
 
+class CodeReview(Thread):
+    commit_sha: str
+    filepath: str
+    status: typing.Literal["APPROVE", "REQUEST_CHANGES", "COMMENT"]
+
+    start_line_number: int
+    end_line_number: typing.Optional[int] = None
+
+    def to_str(self):
+        return f"{self.commit_sha}\n" \
+               f"{self.filepath}:L{self.start_line_number}" + f"{f'-L{self.end_line_number}' if self.end_line_number else ''}\n" \
+               f"{self.status}\n\n" + "\n".join(message.to_str() for message in self.messages)
+
+
+class PullRequest(Issue):
+    base_branch: str
+    head_branch: str
+    code_review_threads: list[CodeReview]
+
+    def to_str(self):
+        return f"#{self.number} {self.title}\n\n" + "\n".join(
+            message.to_str() for message in self.messages
+        ) + "\n\n" + "\n".join(
+            thread.to_str() for thread in self.code_review_threads
+        )
+
+
 DiffStr: TypeAlias = str
--- autopr/models/events.py
+++ autopr/models/events.py
@@ -2,7 +2,7 @@ from typing import Literal, Union
 
 import pydantic
 
-from autopr.models.artifacts import Issue, Message
+from autopr.models.artifacts import Issue, Message, PullRequest, CodeReview
 
 
 class Event(pydantic.BaseModel):
@@ -19,6 +19,21 @@ class IssueCommentEvent(Event):
     event_type: Literal['issue_closed'] = 'issue_closed'
 
     issue: Issue
+    new_comment: Message
+
+
+class PullRequestCommentEvent(Event):
+    event_type: Literal['pull_request_comment'] = 'pull_request_comment'
+
+    pull_request: PullRequest
+    new_comment: Message
+
+
+class CodeReviewCommentEvent(Event):
+    event_type: Literal['code_review_comment'] = 'code_review_comment'
+
+    pull_request: PullRequest
+    code_review: CodeReview
     new_comment: Message
 
 
